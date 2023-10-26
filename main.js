50420a7d3f473c11ee4a47407b30f4d96024aab0
shannoj <69207315+shannoj@users.noreply.github.com>
2023-10-26 13:51:50 +0200

    added left button functionality and implemented modulo function

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 rightButton = document.getElementById('move-right');
-leftButton = document.getElementById('left-button');
+leftButton = document.getElementById('move-left');
 photoContainer = document.getElementById('photo-container');
 dot1 = document.getElementById('dot-1');
 dot2 = document.getElementById('dot-2');
 dot3 = document.getElementById('dot-3');
 
+function mod(n, m) {
+    return ((n % m) + m) % m;
+  }
+
 let x = 1;
-let positionCounter = (x % 4);
+let positionCounter = mod(x, 4);
+dot1.style.backgroundColor = 'white';
+dot2.style.backgroundColor = 'grey';
+dot3.style.backgroundColor = 'grey';
 
 rightButton.addEventListener('click', function(){
+    x += 1;
+    positionCounter = mod(x, 4);
+    if (positionCounter == 0){
+        x +=1;
+        positionCounter = mod(x, 4);
+    }
     console.log(positionCounter);
-    console.log(x);
     if (positionCounter == 1){
         dot1.style.backgroundColor = 'white';
         dot2.style.backgroundColor = 'grey';
@@ -24,14 +36,27 @@ rightButton.addEventListener('click', function(){
         dot2.style.backgroundColor = 'grey';
         dot3.style.backgroundColor = 'white';
     }
-    x += 1;
-    positionCounter = (x % 4);
-    if (positionCounter == 0){
-        positionCounter += 1;
-        x +=1;
-    }
 })
 
 leftButton.addEventListener('click', function(){
-
+    x -= 1;
+    positionCounter = mod(x, 4);
+    if (positionCounter == 0){
+        x -=1;
+        positionCounter = mod(x,4);
+    }
+    console.log(positionCounter);
+    if (positionCounter == 1){
+        dot1.style.backgroundColor = 'white';
+        dot2.style.backgroundColor = 'grey';
+        dot3.style.backgroundColor = 'grey';
+    } else if (positionCounter == 2){
+        dot1.style.backgroundColor = 'grey';
+        dot2.style.backgroundColor = 'white';
+        dot3.style.backgroundColor = 'grey';
+    } else if (positionCounter == 3){
+        dot1.style.backgroundColor = 'grey';
+        dot2.style.backgroundColor = 'grey';
+        dot3.style.backgroundColor = 'white';
+    }
 })
